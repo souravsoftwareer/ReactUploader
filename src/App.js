6348c13efad97f17ed9cf62054270061b6d76113
sourav_kumarr <nk3527@gmail.com>
2024-02-28 00:13:34 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {
-  Col, Row, Upload
+  Col, Row, Button
 } from 'antd'
 import { useState } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
@@ -17,74 +17,48 @@ const getBase64 = (file) =>
 function App() {
 
   const [fileList, setFileList] = useState([])
-  const [previewOpen, setPreviewOpen] = useState(false);
-  const [previewImage, setPreviewImage] = useState('');
-  const [previewTitle, setPreviewTitle] = useState('');
 
-  const beforeUpload = (file, fileList) => {
-    if (fileList.includes(file.name)) {
-      console.log(`${file.name} is disabled.`);
-      return false; // Disable the file
-    }
-    return true; // Enable other files
+
+
+  const handleFileChange = (e) => {
+    const files = Array.from(e.target.files);
+    setFileList(files);
   };
 
-  const handlePreview = async (file) => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj);
+  const uploadFiles=()=>{
+    if(fileList.length == 0) {
+      alert("No files choosen to upload !!!!")
+      return
     }
-    setPreviewImage(file.url || file.preview);
-    setPreviewOpen(true);
-    setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
-  };
-  const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
-  console.log("file list", fileList)
-  const uploadButton = (
-    <button
-      style={{
-        border: 0,
-        background: 'none',
-      }}
-      type="button"
-    >
-      <PlusOutlined />
-      <div
-        style={{
-          marginTop: 8,
-        }}
-      >
-        Upload
-      </div>
-    </button>
-  );
+  }
+  
+  const clearFiles=()=>{
+    alert("Files are cleared !!")
+  }
 
 
   return (
 
     <>
       <Row style={{ margin: 15 }}>
-        <Upload
-          action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
-          listType="picture-card"
-          // fileList={fileList}
-          onPreview={handlePreview}
-          onChange={handleChange}
-          multiple
-          // beforeUpload={beforeUpload}
-        >
-          {fileList.length >= 8 ? null : uploadButton}
-        </Upload>
+        <Col md={24} sm={24} xs={24} lg={24}>
+          <div className={'d-flex flex-row'}>
+            <input type="file" multiple onChange={handleFileChange} />
+            <Button type="primary" onClick={() => uploadFiles()}>Upload</Button>
+            <Button type="primary" danger onClick={() => clearFiles()} style={{ marginLeft:10 }}>Clear Data</Button>
+          </div>
+        </Col>
       </Row>
       <Row gutter={0}>
-        {fileList.map((item) => {
-          
+        {fileList.map((item, index) => {
+
           return (
 
             <Col md={3} lg={3} sm={24} xs={24} style={{ margin: 15 }}>
               <img
                 alt="example"
-                style={{ borderRadius: 5, objectFit: 'contain', width: '100%' }}
-                src={item.thumbUrl}
+                style={{ borderRadius: 5, objectFit: 'cover', height: 200, width: '100%' }}
+                src={URL.createObjectURL(item)} alt={`file-${index}`}
               />
               <p>Upload Status</p>
             </Col>
